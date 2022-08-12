112cc7c62cf9c869ae984f4ff02fbde29b2e7e6b
Idevilson Junior <idevilsonjunior@outlook.com>
2022-08-12 08:38:19 -0300

    First Commit

---
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -0,0 +1,25 @@
+import { ISpecificationRepository } from "../../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateSpecificationUseCase {
+  constructor(private specificationsRepository: ISpecificationRepository) {}
+  execute({ name, description }: IRequest): void {
+    const speficicationAlreadExists =
+      this.specificationsRepository.findByName(name);
+
+    if (speficicationAlreadExists) {
+      throw new Error("Specification already exists!");
+    }
+
+    this.specificationsRepository.create({
+      name,
+      description,
+    });
+  }
+}
+
+export { CreateSpecificationUseCase };
